e4760398d0464072262eff64fa4aab1196ac89d7
ravilella1234 <masters.selenium@gmail.com>
2023-03-10 08:47:12 +0530

    2D- Arrays & User define arrays

--- Feb2023_8AMBatch/src/icici/loans/homeloans/Student.java
+++ Feb2023_8AMBatch/src/icici/loans/homeloans/Student.java
@@ -1,5 +1,29 @@
 package icici.loans.homeloans;
 
-public class Student {
+public class Student 
+{
+	int sno;
+	String sname;
+	
+	public Student(int sno, String sname) 
+	{
+		super();
+		this.sno = sno;
+		this.sname = sname;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return "Student [sno=" + sno + ", sname=" + sname + "]";
+	}
+
+	public static void main(String[] args) 
+	{
+		Student s = new Student(100, "sai");
+		//System.out.println(s.sno);
+		//System.out.println(s.sname);
+		System.out.println(s);
+	}
 
 }
